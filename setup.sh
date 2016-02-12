a7b656a176bc5b8ed25ca36109ce03e6ebec414b
Jun <juns@Juns-MBP-2.local>
2016-02-12 20:47:20 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+# List of symlinks.
+SYMLINK_LIST="
+.vim
+.vimrc
+.gitconfig
+.tmux.conf
+.tmux-powerlinerc
+"
+
+# Get the path of this script, and move there.
+BASE_DIR=$(cd $(dirname $0);pwd)
+cd $BASE_DIR
+
+# Symlinks.
+for FILE in $SYMLINK_LIST; do
+    ln -sf $BASE_DIR/$FILE ~/$FILE
+done
+
+if [ ! -d $HOME/.zprezto ] ; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
+setopt EXTENDED_GLOB
+for rcfile in $HOME/.zprezto/runcoms/^README.md(.N); do
+  echo "update-symbolic-link: => $rcfile"
+  ln -sf "$rcfile" "$HOME/.${rcfile:t}"
+done
