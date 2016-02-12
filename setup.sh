3777060108e28e44efc46ed7b6f7250735c4fbc6
Jun <juns@Juns-MBP-2.local>
2016-02-12 22:14:43 +0900

    modified

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 # List of symlinks.
-SYMLINK_LIST=( "bin" ".vimrc" ".gitconfig" ".zshrc" )
+SYMLINK_LIST=( ".vimrc" ".gitconfig" ".zshrc" )
 
-
+############################################################
+# install zprezto
 if [ ! -d $HOME/.zprezto ] ; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
@@ -14,6 +15,15 @@ for rcfile in $HOME/.zprezto/runcoms/^README.md(.N); do
   ln -sf "$rcfile" "$HOME/.${rcfile:t}"
 done
 
+############################################################
+# vim-neobundle
+
+if [ ! -d $HOME/.vim ] ; then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
+############################################################
 # Get the path of this script, and move there.
 BASE_DIR=$(cd $(dirname $0);pwd)
 cd $BASE_DIR
@@ -22,4 +32,8 @@ cd $BASE_DIR
 for FILE in $SYMLINK_LIST; do
     ln -sf $BASE_DIR/$FILE ~/$FILE
 done
+
+if [ ! -d $HOME/bin ] ; then
+    ln -s $BASE_DIR/$FILE ~/$FILE
+fi
 
